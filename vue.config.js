cf0859a6603d9f531b61b8af7ded65049a2a2c94
admin <admin@admindeAir.lan>
2022-10-19 21:31:45 +0800

    feat:默认样式设置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  //设置css样式主入口文件
+  css:{
+    loaderOptions:{
+      scss:{
+        additionalData:`@import "./src/styles/style.scss";`
+      }
+    }
+  }
 })

0aa1b4275ce3bcc3895365a485fb09b9301aac26
admin <admin@admindeAir.lan>
2022-10-20 10:02:20 +0800

    feat:自动按需引入

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
@@ -9,5 +12,15 @@ module.exports = defineConfig({
         additionalData:`@import "./src/styles/style.scss";`
       }
     }
+  },
+  configureWebpack:{
+    plugins:[
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ]
   }
 })

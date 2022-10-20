2e8792a6bdf52e49ef8c39c77c5c78545ec9f4a5
admin <admin@admindeAir.lan>
2022-10-20 11:11:35 +0800

    feat:登录表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,22 +1,182 @@
 <template>
   <div class="login">
-    登录页
-    <el-button type="success">Success</el-button>
+    <div class="login-con">
+      <ul class="menu-tab">
+        <li
+          v-for="item in MenuData"
+          :class="{ current: item.current }"
+          :key="item.type"
+          @click="MenuClick(item)"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+      <!-- 表单部分 -->
+      <el-form
+        ref="ruleFormRef"
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        class="demo-ruleForm"
+      >
+        <el-form-item prop="pass">
+          <label>邮箱</label>
+          <el-input
+            v-model="ruleForm.pass"
+            type="password"
+            autocomplete="off"
+          />
+        </el-form-item>
+        <el-form-item prop="checkPass">
+          <label>密码</label>
+          <el-input
+            v-model="ruleForm.checkPass"
+            type="password"
+            autocomplete="off"
+          />
+        </el-form-item>
+        <el-form-item prop="age">
+          <label>验证密码</label>
+          <el-input v-model.number="ruleForm.age" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="login-btn block" @click="submitForm(ruleFormRef)"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts" setup>
+import { reactive } from "vue";
+import { ref } from "vue";
+import type { FormInstance } from "element-plus";
 
-}
+const MenuData = reactive([
+  { txt: "登录", current: true, type: "login" },
+  { txt: "注册", current: false, type: "login" },
+]);
+
+let MenuClick = (e: any) => {
+  MenuData.forEach((el) => {
+    el.current = false;
+  });
+  e.current = true;
+};
+// 表单
+const ruleFormRef = ref<FormInstance>();
+
+const checkAge = (rule: any, value: any, callback: any) => {
+  if (!value) {
+    return callback(new Error("Please input the age"));
+  }
+  setTimeout(() => {
+    if (!Number.isInteger(value)) {
+      callback(new Error("Please input digits"));
+    } else {
+      if (value < 18) {
+        callback(new Error("Age must be greater than 18"));
+      } else {
+        callback();
+      }
+    }
+  }, 1000);
+};
+
+const validatePass = (rule: any, value: any, callback: any) => {
+  if (value === "") {
+    callback(new Error("Please input the password"));
+  } else {
+    if (ruleForm.checkPass !== "") {
+      if (!ruleFormRef.value) return;
+      ruleFormRef.value.validateField("checkPass", () => null);
+    }
+    callback();
+  }
+};
+const validatePass2 = (rule: any, value: any, callback: any) => {
+  if (value === "") {
+    callback(new Error("Please input the password again"));
+  } else if (value !== ruleForm.pass) {
+    callback(new Error("Two inputs don't match!"));
+  } else {
+    callback();
+  }
+};
+
+const ruleForm = reactive({
+  pass: "",
+  checkPass: "",
+  age: "",
+});
+
+const rules = reactive({
+  pass: [{ validator: validatePass, trigger: "blur" }],
+  checkPass: [{ validator: validatePass2, trigger: "blur" }],
+  age: [{ validator: checkAge, trigger: "blur" }],
+});
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log("submit!");
+    } else {
+      console.log("error submit!");
+      return false;
+    }
+  });
+};
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.resetFields();
+};
 </script>
 
 <style lang="scss">
-  html,body,#app{
-    height: 100%;
+html,
+body,
+#app {
+  height: 100%;
+}
+.login {
+  background-color: #000066;
+  height: 100%;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #ffffff;
+    border-radius: 2px;
+    cursor: pointer;
   }
-  .login{
-    background-color: #000066;
-    height: 100%;
+  .current {
+    background-color: rgba(255, 255, 255, 0.5);
   }
+}
+.demo-ruleForm {
+  width: 30%;
+  margin: 50px auto;
+  label {
+    display: block;
+    margin-bottom: 3px;
+    font-style: 14px;
+    color: #fff;
+  }
+  .block{
+    display: block;
+    width: 100%;
+  }
+  .login-btn{
+  margin-top: 20px;
+}
+}
+
 </style>

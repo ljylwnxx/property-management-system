9ee0d026663c38500e2141752f9665bf9db792d7
admin <admin@admindeAir.lan>
2022-10-21 17:01:40 +0800

    feat:侧边栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <LeftMenu/>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup>
-
+import LeftMenu from "../../components/LeftMenu.vue"
 </script>
 
-<style>
-
+<style lang="scss">
+  .el-aside{
+    background-color:  rgb(236, 219, 134);
+  }
+  .el-header{
+    background-color: rgb(109, 197, 247);;
+  }
+  .el-main{
+    background-color: pink;
+  }
+  .el-aside,.el-container,.common-layout,#app,body,html {
+    height: 100%;
+  }
 </style>

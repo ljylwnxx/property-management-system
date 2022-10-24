80e3adf027b6358cd5b9e622bb6889db097eb4be
ljylwnxx <ljylwnxx@users.noreply.github.com>
2022-10-24 12:37:47 +0000

    deploy: 46e66e11fe282cde9f3fe792cf325bfb8f24cc69

---
+++ js/946.ca1521fa.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkproperty_management_system"]=self["webpackChunkproperty_management_system"]||[]).push([[946],{89:function(n,t){t.Z=(n,t)=>{const e=n.__vccOpts||n;for(const[r,u]of t)e[r]=u;return e}},946:function(n,t,e){e.r(t),e.d(t,{default:function(){return a}});var r=e(3396);function u(n,t,e,u,s,c){const o=(0,r.up)("router-view");return(0,r.wg)(),(0,r.iD)("div",null,[(0,r.Wm)(o)])}var s={},c=e(89);const o=(0,c.Z)(s,[["render",u]]);var a=o}}]);
+//# sourceMappingURL=946.ca1521fa.js.map
